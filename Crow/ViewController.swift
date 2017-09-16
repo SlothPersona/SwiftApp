7ab08632105e0ed51df9faa3cffdcc5efbf745f4
Alvin Chow <alvinchaachow@gmail.com>
2017-09-16 16:28:40 -0700

    My First Commit

--- Crow/ViewController.swift
+++ Crow/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Label: UILabel!
+    
+var tapCount = 0
+    
+    @IBAction func ButtonPressed(_ sender: Any) {
+        tapCount = tapCount + 1
+        if tapCount == 10 {
+            Label.text = "10% done..."
+        }
+    
+        if tapCount == 20 {
+            Label.text = "20% done..."
+        }
+        
+    }
+
+    @IBAction func ButtonPressed2(_ sender: Any) {
+        Label.text = "Hack Aborted... Pussy"
+        print("button tapped")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
